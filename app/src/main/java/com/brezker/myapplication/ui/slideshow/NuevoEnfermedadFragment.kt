94ea9be2011cc88d2269c5ef8051121464297467
Brezker <brezker10@gmail.com>
2023-06-14 18:12:13 -0600

    Enfermedades completo

--- app/src/main/java/com/brezker/myapplication/ui/slideshow/NuevoEnfermedadFragment.kt
+++ app/src/main/java/com/brezker/myapplication/ui/slideshow/NuevoEnfermedadFragment.kt
@@ -5,12 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.brezker.myapplication.R
+import com.brezker.myapplication.databinding.FragmentNuevoEnfermedadBinding
+import com.brezker.myapplication.extras.Models
+import com.google.gson.Gson
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM1 = "json_enfermedad"
 private const val ARG_PARAM2 = "param2"
+private var id_enfermedad: Int = 0
+private lateinit var binding: FragmentNuevoEnfermedadBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -19,13 +33,16 @@ private const val ARG_PARAM2 = "param2"
  */
 class NuevoEnfermedadFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
+    private var json_enfermedad: String? = null
     private var param2: String? = null
+
+    private var _binding: FragmentNuevoEnfermedadBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
+            json_enfermedad = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
@@ -35,7 +52,88 @@ class NuevoEnfermedadFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nuevo_enfermedad, container, false)
+        //return inflater.inflate(R.layout.fragment_nuevo_enfermedad, container, false)
+        _binding = FragmentNuevoEnfermedadBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        if(json_enfermedad != null) {
+            var gson = Gson()
+            var objEnfermedad = gson.fromJson(json_enfermedad, Models.Enfermedad::class.java)
+
+            com.brezker.myapplication.ui.slideshow.id_enfermedad = objEnfermedad.id
+            binding.edtNombre.setText(objEnfermedad.nombre)
+            binding.edtTipo.setText(objEnfermedad.tipo)
+            binding.edtDescripcion.setText(objEnfermedad.descripcion)
+        }
+
+        binding.btnGuardar.setOnClickListener{
+            guardarDatos()
+        }
+        binding.btnEliminar.setOnClickListener{
+            eliminarDatos()
+        }
+        return  view
+    }
+
+    fun guardarDatos() {
+        val client = OkHttpClient()
+
+        val formBody: RequestBody = FormBody.Builder()
+            .add("id", id_enfermedad.toString())
+            .add("nombre", binding.edtNombre.text.toString())
+            .add("tipo", binding.edtTipo.text.toString())
+            .add("descripcion", binding.edtDescripcion.text.toString())
+            .build()
+
+        val request = Request.Builder()
+            //.url("http://yourip:8000/api/enfermedad")
+            .url("http://192.168.0.7:8000/api/enfermedad")
+            .post(formBody)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                activity?.runOnUiThread {
+                    Toast.makeText(context, "Ocurrio un error: " + e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                var respuesta = response.body?.string()
+                println(respuesta)
+                activity?.runOnUiThread {
+                    activity?.onBackPressed()
+                }
+            }
+        })
+    }
+
+    fun eliminarDatos(){
+        val client = OkHttpClient()
+
+        val formBody: RequestBody = FormBody.Builder()
+            .add("id", id_enfermedad.toString())
+            .build()
+
+        val request = Request.Builder()
+            //.url("http://yourip:8000/api/enfermedad")
+            .url("http://192.168.0.7:8000/api/enfermedad/delete")
+            .post(formBody)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                activity?.runOnUiThread {
+                    Toast.makeText(context, "Ocurrio un error: " + e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                var respuesta = response.body?.string()
+                println(respuesta)
+                activity?.runOnUiThread {
+                    activity?.onBackPressed()
+                }
+            }
+        })
     }
 
     companion object {
