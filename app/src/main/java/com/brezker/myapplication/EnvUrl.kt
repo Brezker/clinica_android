78b84704dc7966f5e3459e87357135f3f2dad7ae
Brezker <brezker10@gmail.com>
2023-07-05 06:27:05 -0600

    Ya todo funciona con spinners

--- app/src/main/java/com/brezker/myapplication/EnvUrl.kt
+++ app/src/main/java/com/brezker/myapplication/EnvUrl.kt
@@ -2,7 +2,7 @@ package com.brezker.myapplication
 
 class EnvUrl {
     companion object{
-        val UrlVal : String = "192.168.0.14"
+        val UrlVal : String = "192.168.43.228"
     }
 }
 fun main(){
--- app/src/main/java/com/brezker/myapplication/ui/cita/NuevoCitaFragment.kt
+++ app/src/main/java/com/brezker/myapplication/ui/cita/NuevoCitaFragment.kt
@@ -65,13 +65,15 @@ class NuevoCitaFragment : Fragment() {
         _binding = FragmentNuevoCitaBinding.inflate(inflater, container, false)
         val view = binding.root
         obtenerDoctor()
+        obtenerEnfermedad()
+        obtenerPaciente()
         if(json_cita != null) {
             var gson = Gson()
             var objCita = gson.fromJson(json_cita, Models.Cita::class.java)
 
             id_cita = objCita.id
-            binding.edtIdenfermedad.setText(objCita.id_enfermedad)
-            binding.edtIdpaciente.setText(objCita.id_paciente)
+            //binding.edtIdenfermedad.setText(objCita.id_enfermedad)
+            //binding.edtIdpaciente.setText(objCita.id_paciente)
             //binding.edtIddoctor.setText(objCita.id_doctor)
             binding.edtConsultorio.setText(objCita.consultorio)
             binding.edtDomicilioc.setText(objCita.domicilio)
@@ -86,33 +88,21 @@ class NuevoCitaFragment : Fragment() {
         }
         return  view
     }
-    private fun obtenerListaDoctores() {
-        // ... Código para obtener la lista de doctores ...
-
-        val doctorValues = ArrayList<String>()
-
-        val adapter = ArrayAdapter<String>(
-            requireActivity().baseContext,
-            android.R.layout.simple_spinner_item,
-            doctorValues
-        )
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spiDoctor.adapter = adapter
-    }
 
     fun guardarDatos() {
         val client = OkHttpClient()
 
         val formBody: RequestBody = FormBody.Builder()
             .add("id", id_cita.toString())
-            .add("id_enfermedad", binding.edtIdenfermedad.text.toString())
-            .add("id_paciente", binding.edtIdpaciente.text.toString())
+            //.add("id_enfermedad", binding.edtIdenfermedad.text.toString())
+            //.add("id_paciente", binding.edtIdpaciente.text.toString())
             //.add("id_doctor", binding.edtIddoctor.text.toString())
             .add("consultorio", binding.edtConsultorio.text.toString())
             .add("domicilio", binding.edtDomicilioc.text.toString())
             .add("fecha", binding.edtFecha.text.toString())
             .add("id_doctor",idDoctor.toString())
+            .add("id_enfermedad", idEnfermedad.toString())
+            .add("id_paciente", idPaciente.toString())
             .build()
 
         val request = Request.Builder()
@@ -193,7 +183,7 @@ class NuevoCitaFragment : Fragment() {
                         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
                             val view = super.getView(position, convertView, parent)
                             val doctor = listaDoctores[position]
-                            val displayText = "ID: "+"${doctor.id}"
+                            val displayText = "ID Doc: "+"${doctor.id}"
                             //val displayText = "${doctor.id} ${doctor.nombre}"
                             (view as TextView).text = displayText
                             return view
@@ -232,6 +222,164 @@ class NuevoCitaFragment : Fragment() {
                                 val selectedDoctor = listaDoctores[position]
                                 idDoctor = selectedDoctor.id
                                 println(idDoctor)
+                            }
+
+                            override fun onNothingSelected(adapterView: AdapterView<*>) {
+                                // Manejar caso de no selección si es necesario
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    private fun obtenerEnfermedad() {
+        val url = "http://" + EnvUrl.UrlVal + ":8000/api/enfermedades"
+
+        val request = Request.Builder().url(url).get().build()
+        val client = OkHttpClient()
+        val objGson = Gson()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Manejar error en caso de fallo
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val respuesta = response.body?.string()
+
+                val listaEnfermedades = objGson.fromJson(respuesta, Array<Models.Enfermedad>::class.java)
+
+                if (listaEnfermedades != null) {
+                    var selectedPosition = -1
+
+                    val adapter = object : ArrayAdapter<Models.Enfermedad>(
+                        requireActivity().baseContext,
+                        android.R.layout.simple_spinner_item,
+                        listaEnfermedades
+                    ) {
+                        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                            val view = super.getView(position, convertView, parent)
+                            val enfermedad = listaEnfermedades[position]
+                            val displayText = "ID Enf: "+"${enfermedad.id}"
+                            //val displayText = "${enfermedad.id} ${enfermedad.nombre}"
+                            (view as TextView).text = displayText
+                            return view
+                        }
+
+                        override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                            val view = super.getDropDownView(position, convertView, parent)
+                            val enfermedad = listaEnfermedades[position]
+                            //val displayText = "${enfermedad.id} ${enfermedad.nombre}"
+                            val displayText = "${enfermedad.nombre}"
+                            (view as TextView).text = displayText
+                            return view
+                        }
+                    }
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                    activity?.runOnUiThread {
+                        binding.spiEnfermedad.adapter = adapter
+
+                        var gson = Gson()
+                        var objCita = gson.fromJson(json_cita, Models.Cita::class.java)
+                        //println(json_cita.toString())
+                        if (json_cita != null) {
+                            for (i in listaEnfermedades.indices) {
+                                if (objCita.id_enfermedad == listaEnfermedades[i].id.toString()) {
+                                    selectedPosition = i
+                                    binding.spiEnfermedad.setSelection(selectedPosition)
+                                    break
+                                }
+                            }
+                        }
+
+                        binding.spiEnfermedad.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long) {
+                                val selectedEnfermedad = listaEnfermedades[position]
+                                idEnfermedad = selectedEnfermedad.id
+                                println(idEnfermedad)
+                            }
+
+                            override fun onNothingSelected(adapterView: AdapterView<*>) {
+                                // Manejar caso de no selección si es necesario
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    private fun obtenerPaciente() {
+        val url = "http://" + EnvUrl.UrlVal + ":8000/api/pacientes"
+
+        val request = Request.Builder().url(url).get().build()
+        val client = OkHttpClient()
+        val objGson = Gson()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Manejar error en caso de fallo
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val respuesta = response.body?.string()
+
+                val listaPacientes = objGson.fromJson(respuesta, Array<Models.Paciente>::class.java)
+
+                if (listaPacientes != null) {
+                    var selectedPosition = -1
+
+                    val adapter = object : ArrayAdapter<Models.Paciente>(
+                        requireActivity().baseContext,
+                        android.R.layout.simple_spinner_item,
+                        listaPacientes
+                    ) {
+                        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                            val view = super.getView(position, convertView, parent)
+                            val paciente = listaPacientes[position]
+                            val displayText = "ID Pac: "+"${paciente.id}"
+                            //val displayText = "${enfermedad.id} ${enfermedad.nombre}"
+                            (view as TextView).text = displayText
+                            return view
+                        }
+
+                        override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                            val view = super.getDropDownView(position, convertView, parent)
+                            val paciente = listaPacientes[position]
+                            //val displayText = "${enfermedad.id} ${enfermedad.nombre}"
+                            val displayText = "${paciente.nombre}"
+                            (view as TextView).text = displayText
+                            return view
+                        }
+                    }
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                    activity?.runOnUiThread {
+                        binding.spiPaciente.adapter = adapter
+
+                        var gson = Gson()
+                        var objCita = gson.fromJson(json_cita, Models.Cita::class.java)
+                        //println(json_cita.toString())
+                        if (json_cita != null) {
+                            for (i in listaPacientes.indices) {
+                                if (objCita.id_paciente == listaPacientes[i].id.toString()) {
+                                    selectedPosition = i
+                                    binding.spiPaciente.setSelection(selectedPosition)
+                                    break
+                                }
+                            }
+                        }
+
+                        binding.spiPaciente.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long) {
+                                val selectedPaciente = listaPacientes[position]
+                                idPaciente = selectedPaciente.id
+                                println(idPaciente)
                             }
 
                             override fun onNothingSelected(adapterView: AdapterView<*>) {
